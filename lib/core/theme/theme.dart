4c27c113270e77af104f90033b030f1848d986ee
tanmayymaheshwari <tanmay16july@gmail.com>
2024-05-23 15:47:47 +0530

    SignUpUI

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,20 @@
+import 'package:chronicles/core/theme/app_pallete.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static _border([Color color = AppPallete.borderColor]) => OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10),
+    borderSide: BorderSide(
+      color: color,
+      width: 3,
+    ),
+  );
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: AppPallete.backgroundColor,
+    inputDecorationTheme: InputDecorationTheme(
+      enabledBorder: _border(),
+      focusedBorder: _border(AppPallete.gradient1),
+      contentPadding: EdgeInsets.all(27),
+    ),
+  );
+}
---
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -0,0 +1,32 @@
+import 'package:chronicles/features/auth/presentation/widgets/auth_field.dart';
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Sign Up.",
+            style: TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          AuthField(hintText: 'Name',),
+          AuthField(hintText: 'Email',),
+          AuthField(hintText: 'Password',),
+        ],
+      ),
+    );
+  }
+}

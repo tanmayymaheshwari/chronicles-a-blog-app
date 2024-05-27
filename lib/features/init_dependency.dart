931c58dcae58bd0afae99124db5c75d24e1c3b97
tanmayymaheshwari <tanmay16july@gmail.com>
2024-05-27 10:36:11 +0530

    Dependency Injections

---
+++ lib/features/init_dependency.dart
@@ -0,0 +1,46 @@
+import 'package:chronicles/core/secrets/app_secrets.dart';
+import 'package:chronicles/features/auth/data/datasources/auth_supabase_datasource.dart';
+import 'package:chronicles/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:chronicles/features/auth/domain/repository/auth_repository.dart';
+import 'package:chronicles/features/auth/domain/usecases/user_sign_up.dart';
+import 'package:chronicles/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+final serviceLocator = GetIt.instance;
+
+Future<void> initDependencies() async {
+  _initAuth();
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
+  );
+  serviceLocator.registerLazySingleton(() => supabase.client);
+}
+
+void _initAuth() {
+  // AuthSupabaseDatasource is passed manually because GetIt
+  // does not know that AuthSupaImpl is an implementation of
+  // AuthSupaData, which is required in AuthRepoImpl
+
+  serviceLocator.registerFactory<AuthSupabaseDatasource>(
+    () => AuthSupabaseDatasourceImpl(
+      supabaseClient: serviceLocator(),
+    ),
+  );
+  serviceLocator.registerFactory<AuthRepository>(
+    () => AuthRepositoryImpl(
+      remoteDatasource: serviceLocator(),
+    ),
+  );
+  serviceLocator.registerFactory(
+    () => UserSignUp(
+      authRepository: serviceLocator(),
+    ),
+  );
+  serviceLocator.registerLazySingleton(
+    () => AuthBloc(
+      userSignUp: serviceLocator(),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,20 @@
-import 'package:chronicles/core/secrets/app_secrets.dart';
 import 'package:chronicles/core/theme/theme.dart';
-import 'package:chronicles/features/auth/data/datasources/auth_supabase_datasource.dart';
-import 'package:chronicles/features/auth/data/repositories/auth_repository_impl.dart';
-import 'package:chronicles/features/auth/domain/usecases/user_sign_up.dart';
+
 import 'package:chronicles/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:chronicles/features/auth/presentation/pages/login_page.dart';
+import 'package:chronicles/features/init_dependency.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final supabase = await Supabase.initialize(
-    url: AppSecrets.supabaseUrl,
-    anonKey: AppSecrets.supabaseAnonKey,
-  );
+  await initDependencies();
+
   runApp(MultiBlocProvider(
     providers: [
       BlocProvider(
-        create: (_) => AuthBloc(
-          userSignUp: UserSignUp(
-            authRepository: AuthRepositoryImpl(
-              remoteDatasource: AuthSupabaseDatasourceImpl(
-                supabaseClient: supabase.client
-              ),
-            ),
-          ),
-        ),
+        create: (_) => serviceLocator<AuthBloc>()
       ),
     ],
     child: const MyApp(),

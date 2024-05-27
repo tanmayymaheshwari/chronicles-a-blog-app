42ac9a5e36c4d91a6c7557c8f1b02070e015b72c
tanmayymaheshwari <tanmay16july@gmail.com>
2024-05-27 10:08:50 +0530

    SignUp connected to database

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -1,8 +1,10 @@
 import 'package:chronicles/core/theme/app_pallete.dart';
+import 'package:chronicles/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:chronicles/features/auth/presentation/pages/login_page.dart';
 import 'package:chronicles/features/auth/presentation/widgets/auth_field.dart';
 import 'package:chronicles/features/auth/presentation/widgets/auth_gradient_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignUpPage extends StatefulWidget {
   static route() => MaterialPageRoute(
@@ -64,8 +66,19 @@ class _SignUpPageState extends State<SignUpPage> {
                 isObscureText: true,
               ),
               const SizedBox(height: 20),
-              const AuthGradientButton(
+              AuthGradientButton(
                 buttonText: "Sign Up",
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    context.read<AuthBloc>().add(
+                          AuthSignUpEvent(
+                            email: emailController.text.trim(),
+                            password: passwordController.text.trim(),
+                            name: nameController.text.trim(),
+                          ),
+                        );
+                  }
+                },
               ),
               const SizedBox(height: 20),
               GestureDetector(
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:chronicles/core/secrets/app_secrets.dart';
 import 'package:chronicles/core/theme/theme.dart';
+import 'package:chronicles/features/auth/data/datasources/auth_supabase_datasource.dart';
+import 'package:chronicles/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:chronicles/features/auth/domain/usecases/user_sign_up.dart';
+import 'package:chronicles/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:chronicles/features/auth/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
@@ -10,7 +15,22 @@ void main() async {
     url: AppSecrets.supabaseUrl,
     anonKey: AppSecrets.supabaseAnonKey,
   );
-  runApp(const MyApp());
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider(
+        create: (_) => AuthBloc(
+          userSignUp: UserSignUp(
+            authRepository: AuthRepositoryImpl(
+              remoteDatasource: AuthSupabaseDatasourceImpl(
+                supabaseClient: supabase.client
+              ),
+            ),
+          ),
+        ),
+      ),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
